ddf6cd6ea6a177349733de80a31c7f576a5b6dda
Сачков Егор Александрович <sprunkcan@gmail.com>
2025-10-15 16:58:05 +0300

    refactor: Доработан стор и запрос смен

--- src/store/ShiftsStore.ts
+++ src/store/ShiftsStore.ts
@@ -4,6 +4,7 @@ import type { Shift } from "../types";
 
 class ShiftsStore {
   shifts: Shift[] = [];
+  location: { latitude?: number; longitude?: number } | null = null;
   loading = false;
   error: string | null = null;
 
@@ -25,20 +26,28 @@ class ShiftsStore {
     });
   }
 
-  async fetchShiftsNearMe() {
+  async fetchShifts() {
     this.loading = true; this.error = null;
     try {
-      const { lat, lon } = await this.requestLocation();
+      let latitude = this.location?.latitude;
+      let longitude = this.location?.longitude;
+      if (!this.location) {
+        // На эмуляторе геолокация работает странно и определяет где-то в США, для наглядности сделана захардкоженая ссылка
+        // const { lat, lon } = await this.requestLocation();
+        latitude = 45.039268;
+        longitude = 38.987221;
+      }
 
-      const res = await fetch(`https://mobile.handswork.pro/api/shifts/map-list-unauthorized?latitude=${lat}&longitude=${lon}`);
+      const res = await fetch(`https://mobile.handswork.pro/api/shifts/map-list-unauthorized?latitude=${latitude}&longitude=${longitude}`);
       if (!res.ok) throw new Error(`HTTP ${res.status}`);
-      const data = await res.json();
+      const dataList = await res.json();
 
-      const items = Array.isArray(data?.items) ? data.items : (Array.isArray(data) ? data : []);
-
+      const items = dataList?.data || [];
+      console.log(items);
       runInAction(() => {
         this.shifts = items;
         this.loading = false;
+        this.location = { latitude, longitude }
       });
     } catch (e: any) {
       runInAction(() => {
